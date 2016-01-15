c90c99a5a07c1ede298ceadcc8ddd9c2eac6d54d
HongSokHeang <hongsokheang3@gmail.com>
2016-01-15 20:52:09 +0700

    homework

---
+++ Bài Tập/Bài Tập/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bài_Tập
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int hours = 0, minutes, seconds;
+            Console.Write("Please. Enter time as second:");
+            seconds = int.Parse(Console.ReadLine());
+            if (seconds >= 60) ;
+            {
+                minutes = seconds / 60;
+                seconds = seconds % 60;
+                hours = minutes / 60;
+                minutes = minutes % 60;
+                Console.Write("{0}hours {1}minutes {2}seconds", hours, minutes, seconds);
+            }
+            
+
+
+
+
+        }
+    }
+}
